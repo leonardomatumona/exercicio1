b78e8e795441f0d50f09fb6ce005d5f80d62eda3
leonardomatumona <leonardomatumona@gmail.com>
2020-06-22 14:48:45 -0300

    Add project files.

---
+++ exercicio1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Numero1 = 10;
+            int Numero2 = 20;
+
+           int resultado = Numero1 + Numero2;
+
+
+            Console.WriteLine("soma = "  + resultado );
+        }
+    }
+}
